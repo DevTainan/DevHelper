d90918c71c1c9a192203e555bc834d18346ec3ed
chris <chris@kingroupsys.com>
2014-07-14 08:51:15 +0800

    add XmlElement.cs, XmlAttri.cs

---
+++ DevCore/XmlAttri.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevCore
+{
+    class XmlAttri //Attribute
+    {
+        private Dictionary<string, string> m_listAttribute;   //key = name
+        public void SetAttribute(string name, string attributeContent)
+        {
+            m_listAttribute[name] = attributeContent;
+        }
+
+        public string GetContent(string name)
+        {
+            return m_listAttribute[name];
+        }
+
+        public string GetXml(string name)
+        {
+            string str;
+            //如果有name這個值
+            //回傳 attriName="attriContent"
+            str = name + "=\"" + m_listAttribute[name] + "\"";
+            return str;
+        }
+
+        public string GetXml()
+        {
+            string str = "";
+
+            foreach(var attrName in m_listAttribute.Keys)
+            {
+                str += GetXml(attrName) + @" ";
+            }
+            return str;
+        }
+    }
+}
---
+++ DevCore/XmlElement.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevCore
+{
+    class XmlElement
+    {
+        //tag name
+        private string m_tagName;
+        public string Name
+        { 
+            get{ return m_tagName; }
+            set{ m_tagName = value; }
+        }
+
+        //Attribute
+        XmlAttri m_attriList;
+        public string SetAttri(string name, string content)
+        {
+            m_attriList.SetAttribute(name, content);
+        }
+
+        public string GetAttriContent(string name)
+        {
+            m_attriList.GetContent(name);
+        }
+
+        private string GetAttriXml(string name)
+        {
+            return m_attriList.GetXml(name);
+        }
+
+        //output format
+        //1. <tagName attrName="AttrValue">
+        //2. list string
+        public string GetXml()
+        {
+            string str;
+            str = @"<" + m_tagName + " " + m_attriList.GetXml() + @"/>" ;
+            return str;
+        }
+    }
+}
